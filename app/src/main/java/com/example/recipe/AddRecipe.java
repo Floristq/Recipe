ef3766a7345b4445cef9855f80f18b7008e40886
Salman Hossain <codemen.salman@gmail.com>
2021-09-25 19:36:17 +0600

    Invoking image picker

--- app/src/main/java/com/example/recipe/AddRecipe.java
+++ app/src/main/java/com/example/recipe/AddRecipe.java
@@ -1,14 +1,72 @@
 package com.example.recipe;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddRecipe extends AppCompatActivity {
+
+    private FloatingActionButton cameraBtn;
+    private FloatingActionButton galleryBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+
+        cameraBtn = findViewById(R.id.cameraBtn);
+        galleryBtn = findViewById(R.id.galleryBtn);
+
+        getSupportActionBar().setTitle("Add Recipe");
+
+        cameraBtn.setOnClickListener(this::onCameraBtnClick);
+        galleryBtn.setOnClickListener(this::onGalleryBtnClick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void onCameraBtnClick(View view) {
+        invokeImagePicker(view, true);
+    }
+
+    private void onGalleryBtnClick(View view) {
+        invokeImagePicker(view, false);
+    }
+
+    private void invokeImagePicker(View view, Boolean cameraOnly) {
+        ImagePicker.Builder picker = ImagePicker.with(this);
+
+        if (cameraOnly) {
+            picker.cameraOnly();
+        } else {
+            picker.galleryOnly();
+        }
+
+        picker.crop()
+            .compress(1024)
+            .maxResultSize(1080, 1080)
+            .start();
     }
 }
