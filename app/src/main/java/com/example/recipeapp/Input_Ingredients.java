68a5224a0109c5b0ebaf2891a51235c9fd6c552a
Floristq <56862489+Floristq@users.noreply.github.com>
2021-10-02 12:48:55 +1000

    Yang Oct. 2nd

---
+++ app/src/main/java/com/example/recipeapp/Input_Ingredients.java
@@ -0,0 +1,96 @@
+package com.example.recipeapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Input_Ingredients#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Input_Ingredients extends Fragment {
+
+    EditText First_Ingredient;
+    Button button;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Input_Ingredients() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Input_Ingredients.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Input_Ingredients newInstance(String param1, String param2) {
+        Input_Ingredients fragment = new Input_Ingredients();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View v = inflater.inflate(R.layout.fragment_input_ingredients,container,false);
+        button = v.findViewById(R.id.button);
+        First_Ingredient = v.findViewById(R.id.First_Ingredient);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putString("key",First_Ingredient.getText().toString());
+                View_Recipe fragment = new View_Recipe();
+                fragment.setArguments(bundle);
+                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,fragment).commit();
+
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Input Ingredients");
+    }
+}
--- app/src/main/java/com/example/recipeapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/recipeapp/ui/home/HomeFragment.java
@@ -1,55 +1,63 @@
 package com.example.recipeapp.ui.home;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.example.recipeapp.Input_Ingredient;
+import com.example.recipeapp.databinding.FragmentHomeBinding;
+import com.example.recipeapp.Input_Ingredients;
 import com.example.recipeapp.R;
+
+
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+
+
+    FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        Button button = (Button) root.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent in  = new Intent(getActivity(), Input_Ingredient.class);
-                in.putExtra("some","some data");
-                startActivity(in);
-            }
-        });
 
+//        View root = inflater.inflate(R.layout.fragment_home, container, false);
+//        Button button = (Button) root.findViewById(R.id.button);
+//        return root;
 
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
-
-
+        binding = FragmentHomeBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        binding.buttonInputIngredients.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment Input_Ingredients = new Input_Ingredients();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.nav_host_fragment, Input_Ingredients);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+    }
+
+    // to avoid memory leakage
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
