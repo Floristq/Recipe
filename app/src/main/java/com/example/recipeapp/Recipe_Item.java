4a670a17b1d20b8fdb11c6ebc9b0427948e06899
Floristq <56862489+Floristq@users.noreply.github.com>
2021-10-08 03:37:24 +1100

    Yang Oct. 8th. Modify recipe item

--- app/src/main/java/com/example/recipeapp/Recipe_Item.java
+++ app/src/main/java/com/example/recipeapp/Recipe_Item.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +15,13 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Recipe_Item extends Fragment {
+
+    TextView Recipe_Name;
+    TextView Instruction;
+    TextView Comments;
+
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
