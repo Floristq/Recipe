2fa8cb0920d50259bbea18ef5f64d3e2d0d4d44f
Salman Hossain <codemen.salman@gmail.com>
2021-10-11 01:08:53 +0600

    Fetching & displaying recipe item basic information from db

--- app/src/main/java/com/example/recipeapp/Recipe_Item.java
+++ app/src/main/java/com/example/recipeapp/Recipe_Item.java
@@ -5,16 +5,26 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,58 +34,32 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class Recipe_Item extends Fragment {
 
     TextView mNameTextView;
-    TextView mInsturctionTextView;
+    TextView mInstructionTextView;
+    ImageView img;
 
     private Button CommentButton;
 
     public static final String NAME_KEY = "Name";
     public static final String INSTRUCTION_KEY = "Instruction";
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference recipeCollectionRef = db.collection("recipes");
+
     private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("recipes/Greek lemon roast potatoes");
 
     private View root = null;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Recipe_Item() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Recipe_Item.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Recipe_Item newInstance(String param1, String param2) {
-        Recipe_Item fragment = new Recipe_Item();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static Recipe_Item newInstance() {
+        return new Recipe_Item();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-
-
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -86,26 +70,15 @@ public class Recipe_Item extends Fragment {
 
         root = inflater.inflate(R.layout.fragment_recipe_item, container,false);
 
-        mInsturctionTextView = root.findViewById(R.id.Instructions);
+        mInstructionTextView = root.findViewById(R.id.Instructions);
         mNameTextView = root.findViewById(R.id.Recipe_Name);
+        img = root.findViewById(R.id.Image);
 
         CommentButton = root.findViewById(R.id.CommentsButton);
 
         CommentButton.setOnClickListener(this::Go_Comments);
 
-        mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                if (documentSnapshot.exists()){
-                    String NameText = documentSnapshot.getString(NAME_KEY);
-                    String InsturctionText = documentSnapshot.getString(INSTRUCTION_KEY);
-                    mNameTextView.setText(NameText);
-                    mInsturctionTextView.setText(InsturctionText);
-
-                }
-            }
-        });
-
+        loadItem();
 
         return root;
 
@@ -115,6 +88,42 @@ public class Recipe_Item extends Fragment {
         Bundle bundle = new Bundle();
 
         Navigation.findNavController(view).navigate(R.id.comments, bundle);
+    }
+
+    private void loadItem() {
+        Bundle bundle = this.getArguments();
+
+        // We will not load anything if no `id` has been passed
+        if (!bundle.containsKey("id")) return;
+
+        String id = bundle.getString("id");
+        recipeCollectionRef.document(id)
+            .get()
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Map<String, Object> data = document.getData();
+
+                        mNameTextView.setText(String.valueOf(data.get("Name")));                        mNameTextView.setText(String.valueOf(data.get("Name")));
+
+                        Glide.with(img.getContext())
+                                .load(String.valueOf(data.get("Image")))
+                                .placeholder(R.drawable.recipe_placeholder)
+                                .transition(DrawableTransitionOptions.withCrossFade())
+                                .into(img);
+
+                        mInstructionTextView.setText(String.valueOf(data.get("Instruction")));
+
+                    } else {
+                        Toast.makeText(getActivity(), "No item found!", Toast.LENGTH_LONG).show();
+                    }
+
+                    Log.d("Firestore response", String.valueOf(task.getResult()));
+                } else {
+                    Log.d("Firestore failure", "Error getting documents: ", task.getException());
+                }
+            });
     }
 
 
