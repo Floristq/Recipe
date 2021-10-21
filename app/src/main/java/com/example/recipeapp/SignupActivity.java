e14e2c6b9c70979752c355a38af138f119d514fb
HelenJiang9815 <69627131+HelenJiang9815@users.noreply.github.com>
2021-10-21 19:59:23 +0800

    Update SignupActivity.java

--- app/src/main/java/com/example/recipeapp/SignupActivity.java
+++ app/src/main/java/com/example/recipeapp/SignupActivity.java
@@ -1,4 +1,21 @@
 package com.example.recipeapp;
 
-public class SignupActivity {
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
